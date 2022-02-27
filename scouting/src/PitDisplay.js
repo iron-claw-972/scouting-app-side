4a64ecc799ae6520502c81a3f98c256eae8eb5ea
Ashir <ashir.rao26@gmail.com>
2022-02-26 18:29:51 -0800

    New Pit Display for guru pranav

--- scouting/src/PitDisplay.js
+++ scouting/src/PitDisplay.js
@@ -3,6 +3,7 @@ import PdfViewer from "./PdfViewer";
 import SpecTable from "./SpecTable";
 
 import { Header } from "semantic-ui-react";
+import styles from "./Home.module.css";
 
 export default function PitDisplay(props) {
   const autoHeading = "Auto Capabilities";
@@ -30,9 +31,73 @@ export default function PitDisplay(props) {
     <div>
       <Header as="h1">Iron Claw 972-Publicity Board</Header>
       <img src="Screen-Shot-2019-02-15-at-3.24.47-PM-1.png"></img>
+      <img src="Screen-Shot-2019-02-15-at-3.24.47-PM-1.png"></img>
+      <img src="Screen-Shot-2019-02-15-at-3.24.47-PM-1.png"></img>
+      <img src="Screen-Shot-2019-02-15-at-3.24.47-PM-1.png"></img>
+      <img src="Screen-Shot-2019-02-15-at-3.24.47-PM-1.png"></img>
+      <img src="Screen-Shot-2019-02-15-at-3.24.47-PM-1.png"></img>
 
-      <Textbox category={autoHeading} text={autoCapabilities}></Textbox>
       <PdfViewer />
+      <img
+        className={styles.sponsorImage}
+        src={require("./sponsorLogos/newMilleniumFoundation.png")}
+      />
+      <img
+        className={styles.sponsorImage}
+        src={require("./sponsorLogos/aeronet.png")}
+      />
+      <img
+        className={styles.sponsorImage}
+        src={require("./sponsorLogos/google.png")}
+      />
+      <img
+        className={styles.sponsorImage}
+        src={require("./sponsorLogos/campusInsuranceService.png")}
+      />
+      <img
+        className={styles.sponsorImage}
+        src={require("./sponsorLogos/facilitron.png")}
+      />
+      <img
+        className={styles.sponsorImage}
+        src={require("./sponsorLogos/baeSystems.png")}
+      />
+      <img
+        className={styles.sponsorImage}
+        src={require("./sponsorLogos/rotaryClubLosGatos.png")}
+      />
+      <img
+        className={styles.sponsorImage}
+        src={require("./sponsorLogos/qualcomm.png")}
+      />
+      <img
+        className={styles.sponsorImage}
+        src={require("./sponsorLogos/lockheedMartin.png")}
+      />
+      <img
+        className={styles.sponsorImage}
+        src={require("./sponsorLogos/brRacing.png")}
+      />
+      <img
+        className={styles.sponsorImage}
+        src={require("./sponsorLogos/atlas.png")}
+      />
+      <img
+        className={styles.sponsorImage}
+        src={require("./sponsorLogos/intuitiveSurgical.png")}
+      />
+      <img
+        className={styles.sponsorImage}
+        src={require("./sponsorLogos/texasInstruments.png")}
+      />
+      <img
+        className={styles.sponsorImage}
+        src={require("./sponsorLogos/tempCFO.png")}
+      />
+      <img
+        className={styles.sponsorImage}
+        src={require("./sponsorLogos/NowForeverStudios.png")}
+      />
       <Textbox category={"About Us"} text={teamHist}></Textbox>
     </div>
   );
