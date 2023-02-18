1096ca99fa77bfbb4d169e734ff9b9301607d20c
johaj10 <78065218+johaj10@users.noreply.github.com>
2023-02-18 11:33:45 -0800

    Finished CanvasDisplay and modified CanvasChooser

--- src/components/CanvasChooser.js
+++ src/components/CanvasChooser.js
@@ -1,18 +1,25 @@
-import React, { useState, useEffect, useRef } from "react";
+import { get } from "lodash";
+import React, { useEffect, useRef } from "react";
 import field_image from "../img/field_image.png";
 
 const CanvasChooser = ({setMouseCoord, getMouseCoord}) => {
 
+    // Setup canvas element ref
     const canvasRef = useRef(null);
 
+
     useEffect(() => {
+        // Setup canvas element and get context
         const canvas = canvasRef.current;
         const ctx = canvas.getContext('2d');
+
+        // Load field image
         const field_background = new Image();
-        
         field_background.src = field_image;
 
-        const draw = (event = null) => {
+
+
+        const draw = (event = null || getMouseCoord != null) => {
             if (event != null) {
                 let rect = canvas.getBoundingClientRect();
                 let xCoord = event.clientX - rect.left;
@@ -28,13 +35,34 @@ const CanvasChooser = ({setMouseCoord, getMouseCoord}) => {
                 ctx.stroke();
                 setMouseCoord({x: xCoord, y: yCoord});
                 // console.log("Coordinate x: " + Math.round(xCoord), "Coordinate y: " + Math.round(yCoord); // For debugging
+            } else if (getMouseCoord() != null) {
+                ctx.clearRect(0, 0, 360, 180);
+                ctx.drawImage(field_background, 0, 0, 360, 180);
+                
+                ctx.lineWidth = 3;
+                ctx.beginPath();
+                ctx.strokeStyle = 'lime';
+                ctx.arc(getMouseCoord().x, getMouseCoord().y, 8, 0, Math.PI*2);
+                ctx.stroke();
             } else {
-                ctx.drawImage(field_background, 0, 0, 360, 180);  
+                ctx.drawImage(field_background, 0, 0, 360, 180);
             }  
         };
 
+
+
+        /**
+         * Calls draw on load without an event
+         * @see{@link draw}
+         */
         field_background.addEventListener('load', () => draw());
+
+        /**
+         * Calls draw when mouse is clicked and passes the event in
+         * @see{@link draw}
+         */
         canvas.addEventListener('mousedown', (e) => draw(e));
+
     }, []);
 
     return(
--- src/components/CanvasDisplay.js
+++ src/components/CanvasDisplay.js
@@ -1,41 +1,71 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import field_image from "../img/field_image.png";
 
+
+// Sample data for testing
+const sample_data = [{x: 3, y: 100}, {x: 180, y: 40}];
+
+
 const CanvasDisplay = ({data}) => {
+    // Setup ref for canvas
     const canvasRef = useRef(null);
 
+
     useEffect(() => {
+        // Setup canvas element and get context
         const canvas = canvasRef.current;
         const ctx = canvas.getContext('2d');
-        const field_background = new Image();
         
+        // Load field image
+        const field_background = new Image();
         field_background.src = field_image;
 
-        const sample_data = [{x:3, y:100}, {x:20, y:40}];
 
-        const draw = () => {
+        /**
+         * Draws points on field on canvas
+         * @param {Array} points 
+         * Takes in an array of objects. Example:
+         * ```javascript
+         * [{x: 3, y: 100}, {x: 50, y: 40}];
+         * ```
+         */
 
-            for (let coords of data) {
-
-            }
-            let rect = canvas.getBoundingClientRect();
-            // let xCoord = data.mousePos.clientX - rect.left;
-            // let yCoord = data.mousePos.clientY - rect.top;
-    
-            // ctx.clearRect(0, 0, 360, 180);
+        const draw = (points) => {
             ctx.drawImage(field_background, 0, 0, 360, 180);
+            points.forEach(point => drawPoint(point));
+        };
+
+
+
+        /**
+         * Draws a point on field from coordinates
+         * @param {object} coordinates 
+         * ```javascript
+         * {x: 3, y: 100}
+         * ```
+         */
+
+
+        const drawPoint = (coordinates) => {
+            let xCoord = coordinates.x;
+            let yCoord = coordinates.y;
             
             ctx.lineWidth = 3;
             ctx.beginPath();
             ctx.fillStyle = 'red';
-            ctx.arc(10, 30, 5, 0, Math.PI*2);
+            ctx.arc(xCoord, yCoord, 5, 0, Math.PI*2);
             ctx.fill();
-            
-            // console.log("Coordinate x: " + Math.round(xCoord), "Coordinate y: " + Math.round(yCoord); // For debugging  
         };
 
-        field_background.addEventListener('load', () => draw());
-        // canvas.addEventListener('mousedown', (e) => draw(e));
+
+
+        /**
+         * Calls draw on image load
+         * @see{@link draw}
+         */
+        field_background.addEventListener('load', () => {
+            draw(sample_data);
+        });
 
     }, []);
 
